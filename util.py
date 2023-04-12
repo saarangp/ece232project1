be9d576ab8508b7c33c0d5342b45428dac16e377
Mara Pleasure <marapleasure@Maras-MacBook-Pro-2.local>
2023-04-12 15:04:45 -0700

    added undirected er graph function

--- util.py
+++ util.py
@@ -1,1 +1,14 @@
 import numpy as np
+import igraph as ig
+import random
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+### question 1 - part 1
+
+def create_undirected_net(n = 900, p = 0.002, seed = 24):
+    random.seed(seed)
+    return ig.Graph.Erdos_Renyi(n = n, p = p, directed = False, loops = False)
+
+
+    
